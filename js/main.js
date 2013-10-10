e11ed3fe53fef442e3601883ea11eedcc34a4856
Chenlin Huang <chenlin.huang@ele.me>
2013-10-10 19:31:03 +0800

    about done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+  $('#carousel_about').carouFredSel({
+    circular: false,
+    infinite: false,
+    auto: false,
+    items: {
+      visible: 1
+    },
+    pagination: {
+      container: '#pagination_about',
+      anchorBuilder: function(nr) {
+        return '<a class="pagi-item" href="#' + nr + '">' + nr + '</a>';
+      }
+    }
+  });
+});
