cee67e082e4c8ea0f6552e41828be091b0ee4517
Chenlin Huang <chenlin.huang@ele.me>
2013-10-15 18:33:16 +0800

    updating ui

--- js/main.js
+++ js/main.js
@@ -93,20 +93,36 @@ $(document).ready(function() {
     }
   });
 
-  $('#site_nav').on('click', 'a', function(event) {
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 800) {
+      $('#site_header').addClass('fixed');
+    } else {
+      $('#site_header').removeClass('fixed');
+    }
+  });
+
+  $('#site_header').on('click', 'a', function(event) {
     event.preventDefault();
     var target = $(this).attr('href');
+    history.pushState(null, null, target);
     $('html, body').animate({
-      scrollTop: $(target).offset().top - $('#site_header').height()
+      scrollTop: target != "#about" ? $(target).offset().top - $('#site_header').height() : $(target).offset().top
     }, 600);
   });
 
   $('.jobs_list').on('click', 'a', function() {
     var article = $(this).attr('data-detail'),
-        title = $(this).text(),
-        detail = $(article).html();
+        title = $(this).text();
     $('#modal_jobs_title').text(title);
-    $('#modal_jobs_detail').html(detail);
+    $(article).removeClass('hide').siblings('.job_detail').addClass('hide');
     $('#modal_jobs').modal();
+  });
+
+  $('.recruit_detail').on('click', function() {
+    var detail = $(this).attr('data-detail'),
+        title = $(detail).data('title');
+    $('#modal_recruit_detail_title').text(title);
+    $(detail).removeClass('hide').siblings('.recruit_detail_content').addClass('hide');
+    $('#modal_recruit_detail').modal();
   });
 });
