136d6ab1c8a43ee25ae37d6a85bd1c03ade03104
Chenlin Huang <chenlin.huang@ele.me>
2014-05-12 17:16:29 +0800

    ocupation updated

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
       history.pushState(null, null, target);
     }
     $('html, body').animate({
-      scrollTop: target != "#about" ? $(target).offset().top - $('#site_header').height() : $(target).offset().top
+      scrollTop: target != '#about' ? $(target).offset().top - $('#site_header').height() : $(target).offset().top
     }, 600);
   });
 
@@ -35,7 +35,7 @@ $(document).ready(function() {
     }, 600);
   });
 
-  /* init carousel */
+  /* about carousel */
   $('#carousel_about').carouFredSel({
     circular: false,
     infinite: false,
@@ -49,80 +49,15 @@ $(document).ready(function() {
     }
   });
 
-  $('#carousel_campus_dev').carouFredSel({
+  /* recruit carousel */
+  $('#tab_recruit').carouFredSel({
     circular: false,
     infinite: false,
+    direction: 'up',
     auto: false,
     items:  1,
     pagination: {
-      container: '#pagination_campus_dev',
-      anchorBuilder: function(nr) {
-        return '<a class="pagi-item" href="#' + nr + '">' + nr + '</a>';
-      }
-    }
-  });
-
-  $('#carousel_campus_other').carouFredSel({
-    circular: false,
-    infinite: false,
-    auto: false,
-    items:  1,
-    pagination: {
-      container: '#pagination_campus_other',
-      anchorBuilder: function(nr) {
-        return '<a class="pagi-item" href="#' + nr + '">' + nr + '</a>';
-      }
-    }
-  });
-
-  $('#tab_campus').carouFredSel({
-    circular: false,
-    infinite: false,
-    direction: 'down',
-    auto: false,
-    items:  1,
-    pagination: {
-      container: '#tab_nav_campus',
-      anchorBuilder: function() {
-        return '<a class="tab-item">' + $(this).data('categ') + '</a>';
-      }
-    }
-  });
-
-  $('#carousel_social_dev').carouFredSel({
-    circular: false,
-    infinite: false,
-    auto: false,
-    items:  1,
-    pagination: {
-      container: '#pagination_social_dev',
-      anchorBuilder: function(nr) {
-        return '<a class="pagi-item" href="#' + nr + '">' + nr + '</a>';
-      }
-    }
-  });
-
-  $('#carousel_social_other').carouFredSel({
-    circular: false,
-    infinite: false,
-    auto: false,
-    items:  1,
-    pagination: {
-      container: '#pagination_social_other',
-      anchorBuilder: function(nr) {
-        return '<a class="pagi-item" href="#' + nr + '">' + nr + '</a>';
-      }
-    }
-  });
-
-  $('#tab_social').carouFredSel({
-    circular: false,
-    infinite: false,
-    direction: 'down',
-    auto: false,
-    items:  1,
-    pagination: {
-      container: '#tab_nav_social',
+      container: '#tab_nav_recruit',
       anchorBuilder: function() {
         return '<a class="tab-item">' + $(this).data('categ') + '</a>';
       }
